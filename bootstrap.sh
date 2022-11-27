83066d8791cdb7d7895114ab5fbcc42bfe2c31ed
Jan Novak <jan.novak@outlook.com>
2022-11-27 18:27:35 +0100

    remove upgrade

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,6 @@ hostnamectl set-hostname $1
 curl -fsSL https://pkgs.tailscale.com/stable/raspbian/bullseye.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg > /dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/raspbian/bullseye.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 apt update
-apt upgrade
 apt install -y kali-grant-root git xrdp tailscale apt-transport-https dnsmasq
 apt autoremove
 dpkg-reconfigure kali-grant-root
